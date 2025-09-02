e7231da84e96d0d871c188ad80eb62db7e6a26f9
Luiz Eduardo Silva Miguel <00001093133466sp@aluno.educacao.sp.gov.br>
2025-09-02 11:20:37 -0300

    portifólio

---
+++ script.js
@@ -0,0 +1,8 @@
+function logoClick() {
+  alert("Logo e nome clicado!");
+}
+
+function toggleMenu() {
+  document.querySelector(".menu-toggle").classList.toggle("active");
+  alert("Menu clicado!");
+}
